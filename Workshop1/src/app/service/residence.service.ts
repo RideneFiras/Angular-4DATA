483d69cdda4140428c6f23a9fd977458baa4c5c1
RideneFiras <firas.ridene@esprit.tn>
2025-04-25 15:35:17 +0100

    Add service and change to dynamic display

---
+++ Workshop1/src/app/service/residence.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Residence } from '../core/models/residence.model'; // import the model
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResidenceService {
+
+  constructor(private http:HttpClient) { }
+ 
+
+getallResidence():Observable<Residence[]>{
+  return this.http.get<Residence[]>("http://localhost:3000/residence");
+}
+getResidence(id:any):Observable<Residence[]>{
+  return this.http.get<Residence[]>("http://localhost:3000/residence"+"/"+id);
+}
+addResidence(res:Residence):Observable<Residence[]>{
+  return this.http.post<Residence[]>("http://localhost:3000/residence",res);
+}
+updateResidence(id:any,res:Residence):Observable<Residence[]>{
+  return this.http.put<Residence[]>("http://localhost:3000/residence"+"/"+id,res);
+}
+deleteResidence(id:any):Observable<Residence[]>{
+  return this.http.delete<Residence[]>("http://localhost:3000/residence"+"/"+id);
+}
+
+}
